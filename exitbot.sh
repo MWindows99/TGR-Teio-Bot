1310269cd10cc05342bfbf8b592f49e2a5f3f905
うぃーうぉん <80945906+MWindows99@users.noreply.github.com>
2021-08-28 00:11:52 +0900

    Add files via upload

---
+++ exitbot.sh
@@ -0,0 +1,2 @@
+ps -ef
+echo kill pid
---
+++ mainrun.py
@@ -0,0 +1,19 @@
+import time
+
+def gettimeline():
+  print('Getting Twitter TimeLine... Please wait.')
+  time.sleep(1)
+  import gettimeline
+def runwc():
+  print('Making WordCloud PNG File... Please wait.')
+  time.sleep(1)
+  import wc
+
+print('System Start!')
+print('Starting Process...')
+time.sleep(1)
+
+gettimeline()
+runwc()
+
+print('Process Done.')
